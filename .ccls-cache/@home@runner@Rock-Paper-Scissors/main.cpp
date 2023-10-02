a00857eef0b0f89f465a42a9f54c15449221801c
jonnyfrias02 <25848881-jonnyfrias02@users.noreply.replit.com>
2023-10-02 03:59:09 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Rock-Paper-Scissors/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+  char p1, p2, Answer; 
+  do{
+      cout << "Let's Play Rock Paper Scissors!" << endl; 
+      cout << "Player1: Choose R/P/S" << endl; 
+      cin >> p1; 
+      cout << "Player2: Choose R/P/S" << endl;
+      cin >> p2; 
+     //conditions
+  
+     if ((p1 == 'R' && p2 == 'S') || (p1 == 'P' && p2 == 'R') || (p1 == 'S' && p2 == 'P'))
+      cout << "Player 1 Wins!" << endl;
+      //combine statements as single statement for all possibilities of player 1 winning
+    else if ((p1 == 'R' && p2 == 'P') || (p1 == 'P' && p2 == 'S') || (p1 == 'S' && p2 == 'R'))
+      cout << "Player 2 Wins!" << endl; 
+      //same thing as player 1
+  
+    else if (p1 == p2)
+      cout << "Tie!" << endl; 
+    else 
+      cout << "Incorrect Input! Try Again!" << endl; 
+    cout << endl;
+  
+    cout << "Do you wish to continue? (Y/N)" << endl;
+    cin >> Answer;
+    cout << endl;
+  }while(Answer != 'N' && Answer != 'n');
+  //if player one puts same value as p2 it's a tie
+
+}
